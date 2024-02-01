02655dd60884a4852b974ce34ec1950c74dbc4f8
이서인 <iseoin@iseoin-ui-MacBookAir.local>
2024-02-01 21:40:22 +0900

    feat: 버튼 클릭 이벤트 기능 추가

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import { Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
 
+const winRule = {
+  rock: 'scissor',
+  scissor: 'paper',
+  paper: 'rock',
+};
+
+const getResult = function (left, right) {
+  if (winRule[left] === right) return '승리';
+  if (left === winRule[right]) return '패배';
+  return '무승부';
+};
+const handleClick = function () {
+  console.log('가위바위보!');
+};
+
+const me = 'rock';
+const other = 'scissor';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <h1 id="title">가위바위보</h1>
-    <button className="hand">가위</button>
-    <button className="hand">바위</button>
-    <button className="hand">보</button>
+    <h2>{getResult(me, other)}</h2>
+    <button onClick={handleClick} className="hand">
+      가위
+    </button>
+    <button onClick={handleClick} className="hand">
+      바위
+    </button>
+    <button onClick={handleClick} className="hand">
+      보
+    </button>
   </>
 );
