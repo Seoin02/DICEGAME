ec61e9be56aa16b46603573f0d4ff8ec436b762f
이서인 <iseoin@iseoin-ui-MacBookAir.local>
2024-02-01 22:17:24 +0900

    feat: 주사위 이미지 삽입

--- src/index.js
+++ src/index.js
@@ -1,37 +1,5 @@
-import { Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
-
-const winRule = {
-  rock: 'scissor',
-  scissor: 'paper',
-  paper: 'rock',
-};
-
-const getResult = function (left, right) {
-  if (winRule[left] === right) return '승리';
-  if (left === winRule[right]) return '패배';
-  return '무승부';
-};
-const handleClick = function () {
-  console.log('가위바위보!');
-};
-
-const me = 'rock';
-const other = 'scissor';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <>
-    <h1 id="title">가위바위보</h1>
-    <h2>{getResult(me, other)}</h2>
-    <button onClick={handleClick} className="hand">
-      가위
-    </button>
-    <button onClick={handleClick} className="hand">
-      바위
-    </button>
-    <button onClick={handleClick} className="hand">
-      보
-    </button>
-  </>
-);
+root.render(<App />);
