1a4d4e52becccd064f8bb1e9e93d12fef6905c4c
이서인 <seoin2269@naver.com>
2024-02-05 10:50:54 +0900

    feat: 버튼 속성 추가

--- src/Button.js
+++ src/Button.js
@@ -1,5 +1,33 @@
-function Button({ children, onClick }) {
-  return <button onClick={onClick}>{children}</button>;
+const baseButtonStyle = {
+  //CSS 속성 명은 카멜 케이스로 작성해야 한다.
+  padding: '14px 27px',
+  outline: 'none',
+  cursor: 'pointer',
+  borderRadius: '30px',
+  fontSize: '17px',
+};
+
+const blueButtonStyle = {
+  ...baseButtonStyle,
+  backgroundColor: 'rgba(0,89,255,0.2',
+  border: 'solid 1px #7090ff',
+  color: '#7090ff',
+};
+
+const redButtonStyle = {
+  ...baseButtonStyle,
+  backgroundColor: 'rgba(255,89,255,0.2',
+  border: 'solid 1px #ff4664',
+  color: '#ff4664',
+};
+
+function Button({ children, onClick, color }) {
+  const style = color === 'red' ? redButtonStyle : blueButtonStyle;
+  return (
+    <button style={style} onClick={onClick}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
